2e6733b92644667917c0f19e0c6df27cc8de7390
aiokr <hiaiokr@gmail.com>
2023-08-15 23:19:28 +0800

    能根据纯文本的配置文件输出两帧图谱，并且带有过度动画

--- src/app/components/EChartsComponent.jsx
+++ src/app/components/EChartsComponent.jsx
@@ -3,12 +3,15 @@
 import React, { useEffect, useRef } from 'react';
 import * as echarts from 'echarts';
 
-export default function EChartsComponent({ width, height, option }) {
+export default function EChartsComponent({ width, height, option, onChartReady }) {
   const chartRef = useRef(null);
 
   useEffect(() => {
     const chartInstance = echarts.init(chartRef.current);
     chartInstance.setOption(option);
+
+    // 调用 onChartReady 函数，并将图表实例作为参数传递
+    if (onChartReady) onChartReady(chartInstance);
 
     return () => {
       chartInstance.dispose();
@@ -20,6 +23,5 @@ export default function EChartsComponent({ width, height, option }) {
     chartInstance.resize({ width, height });
   }, [width, height]);
 
-  return (<div ref={chartRef} style={{ width: '100%', height: '100%' }} />)
-    ;
+  return (<div ref={chartRef} style={{ width: '100%', height: '100%' }} />);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,77 @@
 "use client"
 
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import EChartsComponent from './components/EChartsComponent';
 
 export function HomePage() {
   const [option, setOption] = useState({
-    title: { text: '默认图表' },
+    title: {
+      text: ''
+    },
+    tooltip: {},
+    legend: {
+      data: ['']
+    },
     xAxis: {
-      type: 'category',
-      data: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+      data: []
     },
-    yAxis: {
-      type: 'value'
-    },
+    yAxis: {},
     series: [
       {
-        data: [7, 5, 5, 7, 5, 7, 7],
-        type: 'bar'
+        name: '',
+        type: 'bar',
+        data: []
       }
     ]
   });
-  const inputRef = useRef();
+  const inputFrame0 = useRef(); //代码编辑器
+  const inputFrame1 = useRef(); //代码编辑器
+  const chartRef = useRef(null);
+  const inputChartType = useRef(); //图谱样式
 
-  const handleClick = () => {
+  useEffect(() => {
+    // 加载第一帧
     try {
-      const newOption = eval(`(${inputRef.current.value})`);
+      const newOption = eval(`(${inputFrame0.current.value})`);
       setOption(newOption);
     } catch (error) {
       console.error('Invalid option:', error);
     }
+
+    // 10秒后加载第二帧
+    const timer = setTimeout(() => {
+      try {
+        const newOption = eval(`(${inputFrame1.current.value})`);
+        setOption(newOption);
+      } catch (error) {
+        console.error('Invalid option:', error);
+      }
+    }, 5000); // 5000毫秒 = 10秒
+
+    // 清除定时器
+    return () => clearTimeout(timer);
+  }, []);
+
+
+  //按下 Reload 按钮重载图谱
+  const handleClick = () => {
+    // 加载第一帧
+    try {
+      const newOption = eval(`(${inputFrame0.current.value})`);
+      chartRef.current.setOption(newOption, true);
+    } catch (error) {
+      console.error('Invalid option:', error);
+    }
+
+    // 10秒后加载第二帧
+    setTimeout(() => {
+      try {
+        const newOption = eval(`(${inputFrame1.current.value})`);
+        chartRef.current.setOption(newOption, true);
+      } catch (error) {
+        console.error('Invalid option:', error);
+      }
+    }, 5000); // 5000毫秒 = 10秒
   };
 
   return (
@@ -36,14 +79,20 @@ export function HomePage() {
       <div className='col-span-6 '>
         <div className='text-xl font-medium text-white pb-2'>Video Area</div>
         <section id='ChartArea' className='aspect-video bg-white p-2'>
-          <EChartsComponent option={option} />
+          <EChartsComponent option={option} onChartReady={chart => chartRef.current = chart} />
         </section>
-        <button className='w-full p-2 mt-2 flex flex-col radius items-center bg-white'>General Video</button>
+        <div className='grid grid-cols-3 gap-2'>
+          <button className='p-2 my-2 flex flex-col items-center bg-white'>Start Rec</button>
+        </div>
       </div>
       <section id="EditArea" className='col-span-6'>
-        <div className='text-xl font-medium text-white pb-2'>Code Editor</div>
-        <textarea ref={inputRef} className='w-full aspect-video bg-white p-2 block' />
-        <button onClick={handleClick} className='w-full p-2 mt-2 flex flex-col radius items-center bg-white'>Reload</button>
+        <div className='text-xl font-medium text-white pb-2'>Code Editor</div><div className='grid grid-cols-3 gap-2'>
+          <textarea ref={inputFrame0} className='w-full aspect-video bg-white p-2 block col-span-3' />
+
+          <textarea ref={inputFrame1} className='w-full aspect-video bg-white p-2 block col-span-3' />
+
+          <button onClick={handleClick} className='p-2 my-2 flex flex-col items-center bg-white col-span-3'>Reload</button>
+        </div>
       </section>
     </main>
   );
