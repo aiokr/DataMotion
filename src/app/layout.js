7f61ed5e106c44418212b6b15e06daad2743a354
aiokr <hiaiokr@gmail.com>
2023-08-17 00:30:42 +0800

    添加 Google 分析

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <div className="container">
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-98EHF8GGMM"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-98EHF8GGMM');
+            `}
+          </Script>
+        </div>
+      </body>
     </html>
   )
 }
